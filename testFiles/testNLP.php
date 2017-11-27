e994af8123d69e719bf91fcb584f3bfa26fd4f59
Nantha-R <rvnanthakumar@gmail.com>
2017-11-27 20:24:50 +0530

    Test file for Algorthm nlp

---
+++ testFiles/testNLP.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+<script src="https://algorithmia.com/v1/clients/js/algorithmia-0.2.0.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <script>
+
+    var input = {
+      "document": "I really like Algorithmia!"
+    };
+    Algorithmia.client("simeqY772qhzCQkDtET++EUGeQv1")
+        .algo("nlp/SentimentAnalysis/1.0.4")
+        .pipe(input)
+        .then(function(output) {
+          console.log(output);
+          document.write(output.result[0].sentiment); 
+        });
+
+
+    </script>
+  </body>
+</html>
